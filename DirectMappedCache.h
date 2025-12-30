0c1e38987ce8fc8fc4e77dd1fdf10b5b0f951c51
mikita12 <143041809+mikita12@users.noreply.github.com>
2025-12-30 01:32:04 +0100

    Update DirectMappedCache.h

--- DirectMappedCache.h
+++ DirectMappedCache.h
@@ -4,20 +4,17 @@
 #include <cstdint>
 #include <vector>
 
-// Wynik dostępu do pamięci podręcznej
 enum class AccessResult {
     Hit,
     Miss,
     MissConflict
 };
 
-// Linia cache
 struct CacheLine {
     uint32_t tag;
     bool valid = false;
 };
 
-// Rozłożenie adresu
 struct Address {
     uint32_t raw;
     uint32_t offset;
@@ -25,13 +22,10 @@ struct Address {
     uint32_t tag;
 };
 
-// Funkcja pomocnicza do obliczania szerokości bitów
 uint32_t bit_width(uint32_t bit);
 
-// Funkcja dekodująca adres na tag, index i offset
 Address decode_address(uint32_t address, uint32_t cache_size, uint32_t cache_line_size, uint32_t address_width);
 
-// Klasa symulująca pamięć podręczną direct-mapped
 class DirectMappedCache {
 private:
     uint32_t cache_size;
@@ -42,8 +36,7 @@ private:
 public:
     DirectMappedCache(uint32_t c_size, uint32_t c_line_size, uint32_t addr_width);
 
-    // Funkcja dostępu do cache: zwraca Hit, Miss lub MissConflict
     AccessResult access(uint32_t address);
 };
 
-#endif // DIRECT_MAPPED_CACHE_H
+#endif
